28e7dd35516a2ea57989e9af85e4ba270804fd6e
kushal <kushaldeveloper@gmail.com>
2015-03-06 10:15:19 -0600

    move my main into main.cpp

--- KPascal/KPascal/RecursiveDescent.cpp
+++ KPascal/KPascal/RecursiveDescent.cpp
@@ -1,12 +1,12 @@
-﻿#include "KParser.h"
-
-int main()
-{
-	KPascal::KParser parser;
-
-	parser.lexer.getToken(parser.token);
-	parser.program();
-	std::cout << "Good code. Press ENTER to quit." << std::endl;
-	std::cin.get();
-	return 0;
-}
+﻿//#include "KParser.h"
+//
+//int main()
+//{
+//	KPascal::KParser parser;
+//
+//	parser.lexer.getToken(parser.token);
+//	parser.program();
+//	std::cout << "Good code. Press ENTER to quit." << std::endl;
+//	std::cin.get();
+//	return 0;
+//}
