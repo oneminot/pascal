f492927b7c4298ea7776b6f2fab18d2a68c2d76f
kushal <kushaldeveloper@gmail.com>
2015-03-16 12:52:34 -0500

    define trie from the pdf

--- testvariables/trie/main.cpp
+++ testvariables/trie/main.cpp
@@ -1,3 +1,15 @@
+#define NR 27 // the American alphabet(26 letters) plus blank.
+typedef char var_type; // the key is a set of characters
+
+typedef struct trie_node{
+	bool NotLeaf;
+	trie_node *pChildren[NR];
+	var_type word[20];
+}node;
+
+trie_node *NewIntern();
+trie_node *NewLeaf(char word[]);
+
 int main()
 {
 	return 0;
