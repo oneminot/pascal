32c559410ef3ada590abcb0d6d7b0526f53583cd
kushal <kushaldeveloper@gmail.com>
2015-04-30 10:32:25 -0500

    break the parser using subtraction

--- KPascal/Kasm/main.cpp
+++ KPascal/Kasm/main.cpp
@@ -15,17 +15,17 @@ int main()
 		push ebp
 		lea eax, DataSegment
 		mov ebp, eax
-		mov eax, 3
-		add eax, 2
-		mov ebx, 5
-		add ebx, 4
-		imul eax, ebx
-		mov ebx, 7
-		add ebx, 6
-		mov ecx, 9
-		add ecx, 8
+		mov eax, 5
+		add eax, 4
+		mov ebx, 2
+		sub ebx, 3
+		mov ecx, 6
+		add ecx, 7
+		mov edx, 8
+		sub edx, 9
+		imul ecx, edx
 		imul ebx, ecx
-		add eax, ebx
+		imul eax, ebx
 		mov [ebp + 0], eax
 		pop ebp
 		pop esp
