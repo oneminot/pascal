b40e58f1846cb4baf3f290281a6e71ad907cd755
kushal <kushaldeveloper@gmail.com>
2015-05-07 14:03:20 -0500

    check two dimensional arrays

--- KPascal/Kasm/main.cpp
+++ KPascal/Kasm/main.cpp
@@ -15,26 +15,3 @@ int main()
 		push ebp
 		lea eax, DataSegment
 		mov ebp, eax
-		mov eax, 5
-		mov [ebp + 0], eax
-		mov eax, [ebp + 0]
-		mov ebx, 6
-		cmp eax, ebx
-		jge  endorelse0
-		mov eax, 42
-		mov [ebp + 0], eax
-		jmp end0
-		endorelse0:
-		end0:
-		pop ebp
-		pop esp
-		pop edi
-		pop esi
-		pop edx
-		pop ecx
-		pop ebx
-		pop eax
-	}
-	std::cin.get();
-	return 0;
-}
